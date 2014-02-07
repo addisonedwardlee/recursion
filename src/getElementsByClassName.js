0e2771003c1dcd7095b6dd891b5f566630e6c9a7
Addison <Addisonedwardlee@gmail.com>
2014-02-06 23:49:27 -0800

    getElements complete

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,9 +1,26 @@
 // If life was easy, we could just do things the easy way:
-// var getElementsByClassName = function (className) {
-//   return document.getElementsByClassName(className);
-// };
-
+/*
+var getElementsByClassName = function (className) {
+   return document.getElementsByClassName(className);
+    };
+*/
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
-  // your code here
+	var a = [];
+	var b = function(child) {
+		var x = child.childNodes;
+		for(var i = 0; i < x.length; i++) {
+			if(x[i].className !== undefined) {
+//what is a better way to handle undefined?
+			if(x[i].className.indexOf(className) !== -1) {
+				a.push(x[i]);
+			};
+			if(x[i].hasChildNodes()) {
+				b(x[i]);
+			}
+		}
+		}
+	}
+	b(document);
+	return a;
 };
