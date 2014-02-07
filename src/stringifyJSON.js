a408fadd5465330d21a497802ec0d6eca1326a96
Addison <Addisonedwardlee@gmail.com>
2014-02-07 12:52:13 -0800

    stringily updates

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,6 +2,30 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to have to write it from scratch:
+
 var stringifyJSON = function (obj) {
-  // your code goes here
+	var a = "";
+	var b = function(x) {
+		if(x === undefined || typeof x === "function") {
+			return
+		} else if (typeof x === "number") {
+			a += (x);
+		} else if (typeof x === "string") {
+			a += ("\"" + x + "\"")
+		} else if(Array.isArray(obj)) {
+	        for(var i = 0; i < obj.length; i++) {
+	          iterator(obj[i], i, obj);
+	    	};
+        } else {
+			a += ("{");
+			for(key in obj) {
+				b(key);
+				a += ":";
+				b(obj.key);
+			};
+			a += ("}");
+		}
+	};
+	b(obj);
+	return a;
 };
